f836855d806256c0be9e1f9e3808b3383f9656bc
Marco Pivetta <ocramius@gmail.com>
2015-11-25 03:16:41 +0100

    Including logic to read `composer.json` sources paths

--- example/test.php
+++ example/test.php
@@ -9,6 +9,23 @@ use PhpParser\ParserFactory;
 (function () {
     require_once  __DIR__ . '/../vendor/autoload.php';
 
+    $getPackageSourceDirs = function ($composerJsonPath) : Traversable {
+        $packageDir   = dirname($composerJsonPath);
+        $composerData = json_decode(file_get_contents($composerJsonPath), true);
+
+        return new ArrayObject(array_values(array_map(
+            function (string $path) use ($packageDir) {
+                return $packageDir . '/' . ltrim($path, '/');
+            },
+            array_merge(
+                $composerData['autoload']['psr-0'] ?? [],
+                $composerData['autoload']['psr-4'] ?? []
+                // @todo support "classmap"
+                // @todo support "files"
+            )
+        )));
+    };
+
     $allFiles          = new LocateAllFilesByExtension();
     $sourcesASTs       = new LocateASTFromFiles((new ParserFactory())->create(ParserFactory::PREFER_PHP7));
     $allDefinedSymbols = new LocateDefinedSymbolsFromASTRoots();
@@ -18,7 +35,7 @@ use PhpParser\ParserFactory;
     $extension   = '.php';
 
     var_dump([
-        'defined' => $allDefinedSymbols($sourcesASTs($allFiles($directories, $extension))),
+        'defined' => $allDefinedSymbols($sourcesASTs($allFiles($getPackageSourceDirs(__DIR__ . '/test-data/zend-feed/composer.json'), $extension))),
         'used'    => $allUsedSymbols($sourcesASTs($allFiles($directories, $extension))),
     ]);
 })();
