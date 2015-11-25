2bbb28143dc3683ee977bf317e388c298d441861
Marco Pivetta <ocramius@gmail.com>
2015-11-25 04:00:58 +0100

    Simplifying source even more (reducing any duplication)

--- example/test.php
+++ example/test.php
@@ -10,22 +10,21 @@ use PhpParser\ParserFactory;
 (function () {
     require_once  __DIR__ . '/../vendor/autoload.php';
 
-    $getPackageSourceFiles                  = new LocateComposerPackageSourceFiles();
-    $getPackageDirectDependenciesSourceFiles = new LocateComposerPackageDirectDependenciesSourceFiles();
+    $getPackageSourceFiles = new LocateComposerPackageSourceFiles();
 
-    $sourcesASTs       = new LocateASTFromFiles((new ParserFactory())->create(ParserFactory::PREFER_PHP7));
-    $allDefinedSymbols = new LocateDefinedSymbolsFromASTRoots();
-    $allUsedSymbols    = new LocateUsedSymbolsFromASTRoots();
-
+    $sourcesASTs  = new LocateASTFromFiles((new ParserFactory())->create(ParserFactory::PREFER_PHP7));
     $composerJson = __DIR__ . '/test-data/zend-feed/composer.json';
 
+    $definedSymbols = (new LocateDefinedSymbolsFromASTRoots())->__invoke($sourcesASTs(
+        (new \ComposerRequireChecker\GeneratorUtil\ComposeGenerators())->__invoke(
+            $getPackageSourceFiles($composerJson),
+            (new LocateComposerPackageDirectDependenciesSourceFiles())->__invoke($composerJson)
+        )
+    ));
+    $usedSymbols = (new LocateUsedSymbolsFromASTRoots())->__invoke($sourcesASTs($getPackageSourceFiles($composerJson)));
+
     var_dump([
-        'defined' => $allDefinedSymbols($sourcesASTs(
-            (new \ComposerRequireChecker\GeneratorUtil\ComposeGenerators())->__invoke(
-                $getPackageSourceFiles($composerJson),
-                $getPackageDirectDependenciesSourceFiles($composerJson)
-            )
-        )),
-        'used'    => $allUsedSymbols($sourcesASTs($getPackageSourceFiles($composerJson))),
+        'defined' => $definedSymbols,
+        'used'    => $usedSymbols,
     ]);
 })();
