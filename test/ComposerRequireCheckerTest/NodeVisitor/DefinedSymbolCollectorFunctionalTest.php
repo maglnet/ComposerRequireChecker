2e2bc325613c49e9869525559cf6e188b6b93748
Marco Pivetta <ocramius@gmail.com>
2015-11-25 01:38:47 +0100

    Basic defined symbols functional test

---
+++ test/ComposerRequireCheckerTest/NodeVisitor/DefinedSymbolCollectorFunctionalTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace ComposerRequireCheckerTest\NodeVisitor;
+
+use ComposerRequireChecker\NodeVisitor\DefinedSymbolCollector;
+use PhpParser\Node;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeTraverserInterface;
+use PhpParser\NodeVisitor\NameResolver;
+use PhpParser\Parser;
+use PhpParser\ParserFactory;
+
+/**
+ * @coversNothing
+ *
+ * @group functional
+ */
+final class DefinedSymbolCollectorFunctionalTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var DefinedSymbolCollector
+     */
+    private $collector;
+
+    /**
+     * @var Parser
+     */
+    private $parser;
+
+    /**
+     * @var NodeTraverserInterface
+     */
+    private $traverser;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $this->collector = new DefinedSymbolCollector();
+        $this->parser    = (new ParserFactory())->create(ParserFactory::PREFER_PHP7);
+        $this->traverser = new NodeTraverser();
+
+        $this->traverser->addVisitor(new NameResolver());
+        $this->traverser->addVisitor($this->collector);
+    }
+
+    public function testWillCollectSymbolsUsedInThisFile()
+    {
+        $this->traverseClassAST(self::class);
+
+        self::assertSameCollectedSymbols(
+            ['DefinedSymbolCollectorFunctionalTest'],
+            $this->collector->getDefinedSymbols()
+        );
+    }
+
+    private function traverseClassAST(string $className) : array
+    {
+        return $this->traverser->traverse(
+            $this->parser->parse(
+                file_get_contents((new \ReflectionClass($className))->getFileName())
+            )
+        );
+    }
+
+    private static function assertSameCollectedSymbols(array $expected, array $actual)
+    {
+        self::assertSame(array_diff($expected, $actual), array_diff($actual, $expected));
+    }
+}
