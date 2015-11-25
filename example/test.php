0e04dab0b9702eff179301073f47585d977f78b0
Marco Pivetta <ocramius@gmail.com>
2015-11-25 03:53:58 +0100

    Moving location of dependency sources to a separate class

--- example/test.php
+++ example/test.php
@@ -2,6 +2,7 @@
 
 use ComposerRequireChecker\ASTLocator\LocateASTFromFiles;
 use ComposerRequireChecker\DefinedSymbolsLocator\LocateDefinedSymbolsFromASTRoots;
+use ComposerRequireChecker\FileLocator\LocateComposerPackageDirectDependenciesSourceFiles;
 use ComposerRequireChecker\FileLocator\LocateComposerPackageSourceFiles;
 use ComposerRequireChecker\UsedSymbolsLocator\LocateUsedSymbolsFromASTRoots;
 use PhpParser\ParserFactory;
@@ -9,26 +10,8 @@ use PhpParser\ParserFactory;
 (function () {
     require_once  __DIR__ . '/../vendor/autoload.php';
 
-    $getPackageSourceFiles = new LocateComposerPackageSourceFiles();
-
-    $getPackageDirectDependenciesSourceDirs = function ($composerJsonPath) use ($getPackageSourceFiles) : Traversable {
-        $packageDir = dirname($composerJsonPath);
-
-        $vendorDirs = array_values(array_map(
-            function (string $vendorName) use ($packageDir) {
-                return $packageDir . '/vendor/' . $vendorName;
-            },
-            array_keys(json_decode(file_get_contents($composerJsonPath), true)['require'] ?? [])
-        ));
-
-        foreach ($vendorDirs as $vendorDir) {
-            if (! file_exists($vendorDir . '/composer.json')) {
-                continue;
-            }
-
-            yield from $getPackageSourceFiles($vendorDir . '/composer.json');
-        }
-    };
+    $getPackageSourceFiles                  = new LocateComposerPackageSourceFiles();
+    $getPackageDirectDependenciesSourceFiles = new LocateComposerPackageDirectDependenciesSourceFiles();
 
     $sourcesASTs       = new LocateASTFromFiles((new ParserFactory())->create(ParserFactory::PREFER_PHP7));
     $allDefinedSymbols = new LocateDefinedSymbolsFromASTRoots();
@@ -38,7 +21,7 @@ use PhpParser\ParserFactory;
     $composerJson = __DIR__ . '/test-data/zend-feed/composer.json';
 
     var_dump([
-        'defined' => $allDefinedSymbols($sourcesASTs($getPackageDirectDependenciesSourceDirs($composerJson), $extension)),
+        'defined' => $allDefinedSymbols($sourcesASTs($getPackageDirectDependenciesSourceFiles($composerJson), $extension)),
         'used'    => $allUsedSymbols($sourcesASTs($getPackageSourceFiles($composerJson), $extension)),
     ]);
 })();
