bcaaa4a84607871a6314c3353ed59aa4e7ab58b7
Marco Pivetta <ocramius@gmail.com>
2015-11-25 02:56:52 +0100

    Moving `DefinedSymbolsLocator` to a separate class

--- example/test.php
+++ example/test.php
@@ -1,46 +1,18 @@
 <?php
 
 use ComposerRequireChecker\ASTLocator\LocateASTFromFiles;
+use ComposerRequireChecker\DefinedSymbolsLocator\LocateDefinedSymbolsFromASTRoots;
 use ComposerRequireChecker\FileLocator\LocateAllFilesByExtension;
-use ComposerRequireChecker\NodeVisitor\DefinedSymbolCollector;
-use ComposerRequireChecker\NodeVisitor\UsedSymbolCollector;
 use ComposerRequireChecker\UsedSymbolsLocator\LocateUsedSymbolsFromASTRoots;
-use PhpParser\NodeTraverser;
-use PhpParser\NodeVisitor\NameResolver;
 use PhpParser\ParserFactory;
 
 (function () {
     require_once  __DIR__ . '/../vendor/autoload.php';
 
-    $allFiles    = new LocateAllFilesByExtension();
-    $sourcesASTs = new LocateASTFromFiles((new ParserFactory())->create(ParserFactory::PREFER_PHP7));
-
-    $definedSymbolsCollector = new DefinedSymbolCollector();
-    $definedSymbolsTraverser = new NodeTraverser();
-
-    $definedSymbolsTraverser->addVisitor(new NameResolver());
-    $definedSymbolsTraverser->addVisitor($definedSymbolsCollector);
-
-    $allDefinedSymbols = function (Traversable $ASTs) use ($definedSymbolsTraverser, $definedSymbolsCollector) : array {
-        $symbols = [];
-
-        foreach ($ASTs as $astRoot) {
-            $definedSymbolsTraverser->traverse($astRoot);
-
-            $symbols = array_merge($symbols, $definedSymbolsCollector->getDefinedSymbols());
-        }
-
-        return $symbols;
-    };
-
-    $usedSymbolsCollector = new UsedSymbolCollector();
-    $usedSymbolsTraverser = new NodeTraverser();
-
-    $usedSymbolsTraverser->addVisitor(new NameResolver());
-    $usedSymbolsTraverser->addVisitor($usedSymbolsCollector);
-
-
-    $allUsedSymbols = new LocateUsedSymbolsFromASTRoots();
+    $allFiles          = new LocateAllFilesByExtension();
+    $sourcesASTs       = new LocateASTFromFiles((new ParserFactory())->create(ParserFactory::PREFER_PHP7));
+    $allDefinedSymbols = new LocateDefinedSymbolsFromASTRoots();
+    $allUsedSymbols    = new LocateUsedSymbolsFromASTRoots();
 
     $directories = new ArrayObject([__DIR__ . '/../src', __DIR__ . '/../test']);
     $extension   = '.php';
---
+++ src/ComposerRequireChecker/DefinedSymbolsLocator/LocateDefinedSymbolsFromASTRoots.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ComposerRequireChecker\DefinedSymbolsLocator;
+
+use ComposerRequireChecker\NodeVisitor\DefinedSymbolCollector;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitor\NameResolver;
+use PhpParser\Parser;
+use Traversable;
+
+final class LocateDefinedSymbolsFromASTRoots
+{
+    /**
+     * @param Traversable|array[] $ASTs a series of AST roots
+     *
+     * @return string[] all the found symbols
+     */
+    public function __invoke(Traversable $ASTs) : array
+    {
+        // note: dependency injection is not really feasible for these two, as they need to co-exist in parallel
+        $traverser = new NodeTraverser();
+
+        $traverser->addVisitor(new NameResolver());
+        $traverser->addVisitor($collector = new DefinedSymbolCollector());
+
+        $astSymbols = [];
+
+        foreach ($ASTs as $astRoot) {
+            $traverser->traverse($astRoot);
+
+            $astSymbols[] = $collector->getDefinedSymbols();
+        }
+
+        return array_values(array_unique(array_merge([], ...$astSymbols)));
+    }
+}
