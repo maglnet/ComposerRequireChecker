8f3e77faad03e83e01ec5c353f669d39712ba130
Marco Pivetta <ocramius@gmail.com>
2015-11-25 01:14:11 +0100

    Basic functional test to check that all used symbols are collected

---
+++ test/ComposerRequireCheckerTest/NodeVisitor/UsedSymbolCollectorFunctionalTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace ComposerRequireChecker\NodeVisitor;
+
+use PhpParser\Node;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeTraverserInterface;
+use PhpParser\Parser;
+use PhpParser\ParserFactory;
+
+/**
+ * @coversNothing
+ *
+ * @group functional
+ */
+final class UsedSymbolCollectorFunctionalTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var UsedSymbolCollector
+     */
+    private $collector;
+
+    /**
+     * @var Parser
+     */
+    private $parser;
+
+    /**
+     * @var NodeTraverserInterface
+     */
+    private $traverser;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $this->collector = new UsedSymbolCollector();
+        $this->parser    = (new ParserFactory())->create(ParserFactory::PREFER_PHP7);
+        $this->traverser = new NodeTraverser();
+
+        $this->traverser->addVisitor($this->collector);
+    }
+
+    public function testWillCollectSymbolsUsedInThisFile()
+    {
+        $this->traverseClassAST(self::class);
+
+        self::assertSameCollectedSymbols(
+            [
+                'ComposerRequireChecker\NodeVisitor\UsedSymbolCollector',
+                'PHPUnit_Framework_TestCase',
+                'PhpParser\NodeTraverser',
+                'PhpParser\ParserFactory',
+                'file_get_contents',
+                'ReflectionClass',
+                'array_diff',
+                'self',
+            ],
+            $this->collector->getCollectedSymbols()
+        );
+    }
+
+    private function traverseClassAST(string $className) : array
+    {
+        return $this->traverser->traverse(
+            $this->parser->parse(
+                file_get_contents((new \ReflectionClass($className))->getFileName())
+            )
+        );
+    }
+
+    private static function assertSameCollectedSymbols(array $expected, array $actual)
+    {
+        self::assertSame(array_diff($expected, $actual), array_diff($actual, $expected));
+    }
+}
