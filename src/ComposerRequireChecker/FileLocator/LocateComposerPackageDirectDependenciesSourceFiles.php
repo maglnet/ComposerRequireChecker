6775fdfcac9fd15304d9534f2a2f5f3f00b7f20d
Marco Pivetta <ocramius@gmail.com>
2015-11-25 03:54:17 +0100

    Moving location of dependency sources to a separate class

---
+++ src/ComposerRequireChecker/FileLocator/LocateComposerPackageDirectDependenciesSourceFiles.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ComposerRequireChecker\FileLocator;
+
+use Generator;
+
+final class LocateComposerPackageDirectDependenciesSourceFiles
+{
+    public function __invoke(string $composerJsonPath) : Generator
+    {
+        $packageDir = dirname($composerJsonPath);
+
+        $vendorDirs = array_values(array_map(
+            function (string $vendorName) use ($packageDir) {
+                return $packageDir . '/vendor/' . $vendorName;
+            },
+            array_keys(json_decode(file_get_contents($composerJsonPath), true)['require'] ?? [])
+        ));
+
+        foreach ($vendorDirs as $vendorDir) {
+            if (! file_exists($vendorDir . '/composer.json')) {
+                continue;
+            }
+
+            yield from (new LocateComposerPackageSourceFiles())->__invoke($vendorDir . '/composer.json');
+        }
+    }
+}
