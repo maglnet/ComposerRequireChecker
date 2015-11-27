a6f5d9ce79a214b59d57e990649fc9c1f6305d98
Matthias Glaub <magl@magl.net>
2015-11-27 11:11:11 +0100

    implement first whitelist draft and defined extension symbols

--- example/test.php
+++ example/test.php
@@ -13,19 +13,31 @@ use PhpParser\ParserFactory;
 (function () {
     require_once  __DIR__ . '/../vendor/autoload.php';
 
+    $whitelistSymbols = ['null', 'true', 'false', 'static', 'self', 'parent'];
+    $defaultExtensions = ["Core","standard"]; // "bcmath","calendar","ctype","date","filter","hash","iconv","json","mcrypt","SPL","pcre","Reflection","session","standard","mysqlnd","tokenizer","zip","zlib","libxml","dom","PDO","Phar","SimpleXML","xml","wddx","xmlreader","xmlwriter"];
+
     $getPackageSourceFiles = new LocateComposerPackageSourceFiles();
 
     $sourcesASTs  = new LocateASTFromFiles((new ParserFactory())->create(ParserFactory::PREFER_PHP7));
     $composerJson = __DIR__ . '/test-data/zend-feed/composer.json';
 
-    $definedSymbols = (new LocateDefinedSymbolsFromASTRoots())->__invoke($sourcesASTs(
+    $definedVendorSymbols = (new LocateDefinedSymbolsFromASTRoots())->__invoke($sourcesASTs(
         (new ComposeGenerators())->__invoke(
             $getPackageSourceFiles($composerJson),
             (new LocateComposerPackageDirectDependenciesSourceFiles())->__invoke($composerJson)
         )
     ));
+
+    $definedExtensionSymbols = (new \ComposerRequireChecker\DefinedSymbolsLocator\LocateDefinedSymbolsFromExtensions())->__invoke($defaultExtensions);
+
     $usedSymbols = (new LocateUsedSymbolsFromASTRoots())->__invoke($sourcesASTs($getPackageSourceFiles($composerJson)));
-    $unknownSymbols = array_diff($usedSymbols, $definedSymbols);
+
+    $unknownSymbols = array_diff(
+        $usedSymbols,
+        $definedVendorSymbols,
+        $definedExtensionSymbols,
+        $whitelistSymbols
+    );
 
     var_dump(['unknown_symbols' => $unknownSymbols]);
 
