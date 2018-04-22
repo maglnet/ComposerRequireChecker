5ee1670656815ef24fe3874fdafc85f69761d0dc
Björn Büttner <Idrinth@users.noreply.github.com>
2018-04-22 15:03:20 +0200

    fixing type issues of tests due to changes in codestructure

--- test/ComposerRequireCheckerTest/UsedSymbolsLocator/LocateUsedSymbolsFromASTRootsTest.php
+++ test/ComposerRequireCheckerTest/UsedSymbolsLocator/LocateUsedSymbolsFromASTRootsTest.php
@@ -3,6 +3,7 @@
 namespace ComposerRequireCheckerTest\UsedSymbolsLocator;
 
 use ArrayObject;
+use ComposerRequireChecker\ASTLocator\FileAST;
 use ComposerRequireChecker\UsedSymbolsLocator\LocateUsedSymbolsFromASTRoots;
 use PhpParser\Node\Name;
 use PhpParser\Node\Stmt\Class_;
@@ -28,7 +29,9 @@ class LocateUsedSymbolsFromASTRootsTest extends TestCase
         $asts = [];
         $symbols = $this->locate($asts);
 
-        $this->assertCount(0, $symbols);
+        $this->assertCount(2, $symbols);
+        $this->assertCount(0, $symbols[0]);
+        $this->assertCount(0, $symbols[1]);
     }
 
     public function testLocate()
@@ -37,8 +40,10 @@ class LocateUsedSymbolsFromASTRootsTest extends TestCase
         $node->extends = new Name('Bar');
         $symbols = $this->locate([[$node]]);
 
-        $this->assertCount(1, $symbols);
-        $this->assertContains('Bar', $symbols);
+        $this->assertCount(2, $symbols);
+        $this->assertCount(1, $symbols[0]);
+        $this->assertContains('Bar', $symbols[0]);
+        $this->assertCount(0, $symbols[1]);
     }
 
     /**
@@ -46,6 +51,6 @@ class LocateUsedSymbolsFromASTRootsTest extends TestCase
      */
     private function locate(array $asts): array
     {
-        return ($this->locator)(new ArrayObject($asts));
+        return ($this->locator)(new FileAST('', new ArrayObject($asts)));
     }
 }
