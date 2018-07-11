79c17cf4f6e67d1209a96b1acf89efd26592c3f6
Matthias Glaub <magl@magl.net>
2018-07-11 20:01:15 +0200

    add documentation and add direct yield

--- src/ComposerRequireChecker/FileLocator/LocateFilesByGlobPattern.php
+++ src/ComposerRequireChecker/FileLocator/LocateFilesByGlobPattern.php
@@ -8,16 +8,17 @@ use Webmozart\Glob\Glob;
 class LocateFilesByGlobPattern
 {
 
-    public function __invoke(array $globPatterns = [], $rootDir) : \Generator
+    /**
+     * @param string[] $globPatterns a list of glob patterns to find files in
+     * @param string $rootDir the root directory that should be used when patterns are relative paths
+     * @return iterable|string[] the files found by the given glob patterns
+     * @see https://github.com/webmozart/glob
+     */
+    public function __invoke(array $globPatterns = [], string $rootDir): iterable
     {
-        $files = [];
-
         foreach ($globPatterns as $globPattern) {
-            $files = Glob::glob(rtrim($rootDir, '/') . '/' . $globPattern);
+            yield from Glob::glob(rtrim($rootDir, '/') . '/' . $globPattern);
         }
-
-
-        yield from $files;
     }
 
 }
