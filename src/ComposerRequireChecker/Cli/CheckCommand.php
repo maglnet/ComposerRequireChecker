7669763b4401d42255c09c87045e6287933fbc73
Björn Büttner <idrinth@users.noreply.github.com>
2018-07-15 17:51:45 +0200

    checking table output for failures

--- src/ComposerRequireChecker/Cli/CheckCommand.php
+++ src/ComposerRequireChecker/Cli/CheckCommand.php
@@ -88,13 +88,24 @@ class CheckCommand extends Command
             throw new \LogicException('There were no symbols found, please check your configuration.');
         }
 
-        $unknownSymbols = array_diff(
-            $usedSymbols,
-            $definedVendorSymbols->getSymbols(),
-            $definedExtensionSymbols,
-            $options->getSymbolWhitelist()
+        return $this->handleResult(
+            array_diff(
+                $usedSymbols,
+                $definedVendorSymbols->getSymbols(),
+                $definedExtensionSymbols,
+                $options->getSymbolWhitelist()
+            ),
+            $output
         );
+    }
 
+    /**
+     * @param array $unknownSymbols
+     * @param OutputInterface $output
+     * @return int
+     */
+    private function handleResult(?array $unknownSymbols, OutputInterface $output): int
+    {
         if (!$unknownSymbols) {
             $output->writeln("There were no unknown symbols found.");
             return 0;
@@ -116,6 +127,10 @@ class CheckCommand extends Command
         return ((int)(bool)$unknownSymbols);
     }
 
+    /**
+     * @param InputInterface $input
+     * @return Options
+     */
     private function getCheckOptions(InputInterface $input): Options
     {
         $fileName = $input->getOption('config-file');
--- test/ComposerRequireCheckerTest/Cli/CheckCommandTest.php
+++ test/ComposerRequireCheckerTest/Cli/CheckCommandTest.php
@@ -3,7 +3,11 @@
 namespace ComposerRequireCheckerTest\Cli;
 
 use ComposerRequireChecker\Cli\Application;
+use ComposerRequireChecker\Cli\CheckCommand;
 use PHPUnit\Framework\TestCase;
+use ReflectionMethod;
+use Symfony\Component\Console\Formatter\OutputFormatterInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Tester\CommandTester;
 
 class CheckCommandTest extends TestCase
@@ -40,5 +44,38 @@ class CheckCommandTest extends TestCase
 
         $this->assertSame(0, $this->commandTester->getStatusCode());
         $this->assertContains('no unknown symbols found', $this->commandTester->getDisplay());
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function testHandleResultForFailure(): void
+    {
+        $command = new CheckCommand();
+        $method = new ReflectionMethod($command, 'handleResult');
+        $method->setAccessible(true);
+        $output = $this->getMockBuilder(OutputInterface::class)->getMock();
+        $output->expects($this->exactly(8))
+            ->method('writeln')
+            ->withConsecutive(
+                ["The following unknown symbols were found:"],
+                ["+--+--+"],
+                ["|<info> unknown symbol </info>|<info> guessed dependency </info>|"],
+                ["+--+--+"],
+                ["| A_Symbol |  |"],
+                ["| A\\Nother\\Symbol |  |"],
+                ["| A_Third\\Symbol |  |"],
+                ["+--+--+"]
+             );
+        $output->expects($this->any())
+            ->method('getFormatter')
+            ->with()
+            ->willReturn($this->getMockBuilder(OutputFormatterInterface::class)->getMock());
+        $this->assertEquals(1, $method->invoke(
+            $command,
+            ['A_Symbol', 'A\\Nother\\Symbol', 'A_Third\\Symbol'],
+            $output
+        ));
     }
 }
