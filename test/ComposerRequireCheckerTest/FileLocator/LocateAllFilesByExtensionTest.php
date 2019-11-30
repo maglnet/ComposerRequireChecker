01b0a91da41df4562d76368f235903048c3c63a8
Guillaume Perréal <guillaume.perreal@irstea.fr>
2019-11-30 16:32:38 +0100

    Fix LocateAllFilesByExtension using a blacklist on Windows.

--- test/ComposerRequireCheckerTest/FileLocator/LocateAllFilesByExtensionTest.php
+++ test/ComposerRequireCheckerTest/FileLocator/LocateAllFilesByExtensionTest.php
@@ -49,6 +49,99 @@ final class LocateAllFilesByExtensionTest extends TestCase
         }
     }
 
+    public function testLocateWithFilenameBlackList(): void
+    {
+        $dir = vfsStream::newDirectory('MyNamespaceA')->at($this->root);
+        for ($i = 0; $i < 10; $i++) {
+            vfsStream::newFile("MyClass$i.php")->at($dir);
+        }
+
+        $foundFiles = $this->locate([$dir->url()], '.php', ['MyClass6']);
+
+        $this->assertCount(9, $foundFiles);
+        $this->assertNotContains(vfsStream::url('MyClass6.php'), $foundFiles);
+    }
+
+    public function testLocateWithDirectoryBlackList(): void
+    {
+        $dir = vfsStream::newDirectory('MyNamespaceA')->at($this->root);
+        for ($i = 0; $i < 10; $i++) {
+            vfsStream::newFile("Directory$i/MyClass.php")->at($dir);
+        }
+
+        $foundFiles = $this->locate([$dir->url()], '.php', ['Directory5/']);
+
+        $this->assertCount(9, $foundFiles);
+        $this->assertNotContains(vfsStream::url('Directory5/MyClass.php'), $foundFiles);
+    }
+
+    /**
+     * @param array $blacklist
+     * @param array $expectedFiles
+     *
+     * @dataProvider provideBlacklists
+     */
+    public function testLocateWithBlackList(array $blacklist, array $expectedFiles): void
+    {
+        $this->root = vfsStream::create([
+            'MyNamespaceA' => [
+                'MyClass.php' => '<?php class MyClass {}',
+                'Foo' => [
+                    'FooClass.php' => '<?php class FooCalls {}',
+                    'Bar' => [
+                        'BarClass.php' => '<?php class BarClass {}',
+                    ]
+                ],
+                'Bar' => [
+                    'AnotherBarClass.php' => '<?php class AnotherBarClass {}',
+                ]
+            ]
+        ]);
+
+        $foundFiles = $this->locate([$this->root->url()], '.php', $blacklist);
+
+        $this->assertCount(count($expectedFiles), $foundFiles);
+        foreach ($expectedFiles as $file) {
+            $this->assertContains($this->root->getChild($file)->url(), $foundFiles);
+        }
+        $this->assertContains($this->root->getChild('MyNamespaceA/Foo/FooClass.php')->url(), $foundFiles);
+    }
+
+    public function provideBlacklists(): array {
+        return [
+            'No blacklist' => [
+                [],
+                [
+                    'MyNamespaceA/MyClass.php',
+                    'MyNamespaceA/Foo/FooClass.php',
+                    'MyNamespaceA/Foo/Bar/BarClass.php',
+                    'MyNamespaceA/Bar/AnotherBarClass.php',
+                ]
+            ],
+            '* wildcard' => [
+                ['Another*.php'],
+                [
+                    'MyNamespaceA/MyClass.php',
+                    'MyNamespaceA/Foo/FooClass.php',
+                    'MyNamespaceA/Foo/Bar/BarClass.php',
+                ]
+            ],
+            '** wildcard' => [
+                ['**/Bar'],
+                [
+                    'MyNamespaceA/MyClass.php',
+                    'MyNamespaceA/Foo/FooClass.php',
+                ]
+            ],
+            'Combined patterns' => [
+                ['My*.php', 'Bar/'],
+                [
+                    'MyNamespaceA/Foo/FooClass.php',
+                ]
+            ],
+        ];
+    }
+
     /**
      * @param string[] $directories
      * @param string $fileExtension
@@ -57,6 +150,10 @@ final class LocateAllFilesByExtensionTest extends TestCase
      */
     private function locate(array $directories, string $fileExtension, ?array $blacklist): array
     {
-        return iterator_to_array(($this->locator)(new ArrayObject($directories), $fileExtension, $blacklist));
+        $files = [];
+        foreach (($this->locator)(new ArrayObject($directories), $fileExtension, $blacklist) as $file) {
+            $files[] = str_replace('\\', '/', $file);
+        }
+        return $files;
     }
 }
