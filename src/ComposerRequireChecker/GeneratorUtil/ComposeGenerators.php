59bb66e1f23b9d7901068c26088c4c15bcb19570
Daniel Badura <d.m.badura@googlemail.com>
2020-07-28 13:15:14 +0200

    more verbose types

--- src/ComposerRequireChecker/GeneratorUtil/ComposeGenerators.php
+++ src/ComposerRequireChecker/GeneratorUtil/ComposeGenerators.php
@@ -6,6 +6,13 @@ use Traversable;
 
 final class ComposeGenerators
 {
+    /**
+     * @template TKey
+     * @template TValue
+     *
+     * @param Traversable<TKey, TValue> ...$generators
+     * @return Traversable<int, TValue>
+     */
     public function __invoke(Traversable ...$generators): Traversable
     {
         foreach ($generators as $generator) {
