0296c64a0ceb6ab7d225d8a7e64e721f155b4227
Thomas Rieschl <thomas@trinet.at>
2020-10-01 22:44:01 +0200

    add a test

--- test/ComposerRequireCheckerTest/Cli/CheckCommandTest.php
+++ test/ComposerRequireCheckerTest/Cli/CheckCommandTest.php
@@ -5,8 +5,14 @@ namespace ComposerRequireCheckerTest\Cli;
 use ComposerRequireChecker\Cli\Application;
 use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Tester\CommandTester;
+
+use function dirname;
+use function file_put_contents;
+use function unlink;
+use function version_compare;
 
 final class CheckCommandTest extends TestCase
 {
@@ -121,5 +127,29 @@ JSON
 
         $this->assertNotEquals(0, $exitCode);
         $this->assertRegExp('/The following unknown symbols were found.*PHPUnit\\\\Framework\\\\TestCase/s', $this->commandTester->getDisplay());
+    }
+
+    public function testNoUnknownSymbolsFound(): void
+    {
+        $baseDir = dirname(__DIR__, 2) . '/fixtures/noUnknownSymbols/';
+        $this->commandTester->execute(['composer-json' => $baseDir . 'composer.json']);
+
+        self::assertSame(Command::SUCCESS, $this->commandTester->getStatusCode());
+        self::assertStringContainsString('There were no unknown symbols found.', $this->commandTester->getDisplay());
+    }
+
+    public function testReservedKeywordInPhp8DoesNotThrowExceptionInPhp7(): void
+    {
+        if (version_compare(PHP_VERSION, '8.0.0') >= 0) {
+            self::markTestSkipped('This test does not work in PHP8');
+        }
+        $baseDir = dirname(__DIR__, 2) . '/fixtures/noUnknownSymbols/';
+        $tmpFile = $baseDir . 'src/Match.php';
+        file_put_contents($tmpFile, '<?php class Match { }');
+
+        $this->commandTester->execute(['composer-json' => $baseDir . 'composer.json']);
+
+        unlink($tmpFile);
+        self::assertSame(Command::SUCCESS, $this->commandTester->getStatusCode());
     }
 }
