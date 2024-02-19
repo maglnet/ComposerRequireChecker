036708d03b5d982462621d612242721294976b44
Dan Wallis <dan@wallis.nz>
2024-02-19 09:25:30 +0000

    Move phar compilation outside of phpunit

--- test/ComposerRequireCheckerTest/PharTest.php
+++ test/ComposerRequireCheckerTest/PharTest.php
@@ -4,18 +4,15 @@ declare(strict_types=1);
 
 namespace ComposerRequireCheckerTest;
 
-use PHPUnit\Framework\Attributes\BeforeClass;
 use PHPUnit\Framework\TestCase;
-use RuntimeException;
 
 use function chdir;
 use function dirname;
+use function escapeshellarg;
 use function exec;
 use function getcwd;
 use function implode;
-use function ob_clean;
-use function ob_end_flush;
-use function ob_start;
+use function realpath;
 use function sprintf;
 
 use const DIRECTORY_SEPARATOR;
@@ -23,35 +20,19 @@ use const PHP_BINARY;
 
 final class PharTest extends TestCase
 {
-    private static string $bin;
+    private string $bin;
     private string $oldWorkingDirectory;
-
-    #[BeforeClass]
-    public static function compilePhar(): void
-    {
-        $doExec           = static function ($command): void {
-            ob_start();
-            exec($command . ' 2>&1', $output, $return);
-            if ($return !== 0) {
-                ob_end_flush();
-
-                throw new RuntimeException('Command `' . $command . '` failed with exit code ' . $return . "\n" . implode("\n", $output));
-            }
-
-            ob_clean();
-        };
-        $workingDirectory = getcwd();
-        chdir(dirname(__DIR__, 2));
-        $doExec('composer --no-interaction install --no-progress');
-        $doExec(PHP_BINARY . ' -d phar.readonly=0 vendor/bin/phing phar-build');
-        $doExec('composer --no-interaction install --no-progress');
-        self::$bin = PHP_BINARY . ' ' . getcwd() . DIRECTORY_SEPARATOR . 'build' . DIRECTORY_SEPARATOR . 'composer-require-checker.phar';
-        chdir($workingDirectory);
-    }
 
     protected function setUp(): void
     {
+        $phar = realpath(dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'build' . DIRECTORY_SEPARATOR . 'composer-require-checker.phar');
+
+        if ($phar === false) {
+            $this->markTestSkipped('Compiled PHAR not found');
+        }
+
         $this->oldWorkingDirectory = getcwd();
+        $this->bin                 = PHP_BINARY . ' ' . escapeshellarg($phar);
     }
 
     protected function tearDown(): void
@@ -65,7 +46,7 @@ final class PharTest extends TestCase
 
     public function testVersion(): void
     {
-        $command = self::$bin . ' --version';
+        $command = $this->bin . ' --version';
         exec($command, $output, $return);
         $this->assertStringContainsString('ComposerRequireChecker', implode("\n", $output));
         $this->assertSame(0, $return);
@@ -73,14 +54,14 @@ final class PharTest extends TestCase
 
     public function testSuccess(): void
     {
-        exec(self::$bin, $output, $return);
+        exec($this->bin, $output, $return);
         $this->assertSame(0, $return);
     }
 
     public function testUnknownSymbols(): void
     {
         $path = __DIR__ . '/../fixtures/unknownSymbols/composer.json';
-        exec(sprintf('%s check %s 2>&1', self::$bin, $path), $output, $return);
+        exec(sprintf('%s check %s 2>&1', $this->bin, $path), $output, $return);
         $this->assertStringContainsString('The following 6 unknown symbols were found', implode("\n", $output));
         $this->assertNotEquals(0, $return);
     }
@@ -88,7 +69,7 @@ final class PharTest extends TestCase
     public function testInvalidConfiguration(): void
     {
         $path = __DIR__ . '/../fixtures/validJson.json';
-        exec(sprintf('%s check %s 2>&1', self::$bin, $path), $output, $return);
+        exec(sprintf('%s check %s 2>&1', $this->bin, $path), $output, $return);
         $this->assertStringContainsString('dependencies have not been installed', implode("\n", $output));
         $this->assertNotEquals(0, $return);
     }
@@ -96,7 +77,7 @@ final class PharTest extends TestCase
     public function testDefaultConfiguration(): void
     {
         chdir(__DIR__ . '/../fixtures/defaultConfigPath');
-        exec(sprintf('%s check 2>&1', self::$bin), $output, $return);
+        exec(sprintf('%s check 2>&1', $this->bin), $output, $return);
         $output = implode("\n", $output);
 
         $this->assertMatchesRegularExpression('/The following [12] unknown symbols were found/', $output);
@@ -107,7 +88,7 @@ final class PharTest extends TestCase
     public function testMissingCustomConfiguration(): void
     {
         chdir(__DIR__ . '/../fixtures/noUnknownComposerSymbol');
-        exec(sprintf('%s check --config-file=%s 2>&1', self::$bin, 'no-such-file'), $output, $return);
+        exec(sprintf('%s check --config-file=%s 2>&1', $this->bin, 'no-such-file'), $output, $return);
 
         $this->assertStringContainsString('Configuration file [no-such-file] does not exist.', implode("\n", $output));
         $this->assertNotEquals(0, $return);
